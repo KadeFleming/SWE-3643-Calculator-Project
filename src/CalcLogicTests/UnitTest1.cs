83c1c5ec550bfe0e6e35c4693abe6ad9f0fe51f8
KadeFleming <kadefle@gmail.com>
2024-12-02 22:20:48 -0500

    Added Location For Logic Unit Tests

---
+++ src/CalcLogicTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CalcLogicTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
